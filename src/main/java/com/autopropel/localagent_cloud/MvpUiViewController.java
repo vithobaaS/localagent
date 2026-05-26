3bc95b825d0f46567deb0d10902a120598e28725
vithobaa <vithobaasrinivas@gmail.com>
2026-05-26 13:17:22 +0530

    ui update

--- src/main/java/com/autopropel/localagent_cloud/MvpUiViewController.java
+++ src/main/java/com/autopropel/localagent_cloud/MvpUiViewController.java
@@ -6,7 +6,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MvpUiViewController {
 
-    @GetMapping("/autopropel/dashboard")
+    @GetMapping({
+        "/autopropel",
+        "/autopropel/",
+        "/autopropel/dashboard",
+        "/autopropel/scheduler",
+        "/autopropel/scheduler/**",
+        "/autopropel/groups",
+        "/autopropel/groups/**",
+        "/autopropel/stub/**",
+        "/autopropel/test-suite",
+        "/autopropel/test-suite/**",
+        "/autopropel/test-case",
+        "/autopropel/test-case/**"
+    })
     public String dashboardRedirect() {
         return "forward:/autopropel/dashboard/index.html";
     }
