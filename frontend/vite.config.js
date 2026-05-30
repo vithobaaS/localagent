f703e3d0edae58aecab95344424793e559098f11
vithobaa <vithobaasrinivas@gmail.com>
2026-05-30 14:51:56 +0530

    dddd

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,9 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/autopropel/',
+  base: '/',
   build: {
-    outDir: '../src/main/resources/static/autopropel',
+    outDir: 'dist',
     emptyOutDir: true
   },
   server: {
